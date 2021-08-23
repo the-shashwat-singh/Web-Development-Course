f2b543b6f816cd637d1f22d9e17bdbb334165c82
Shashwat Singh <shashwatvvrn@gmail.com>
2021-08-23 21:55:39 +0530

    initial commit

---
+++ tut53.js
@@ -0,0 +1,25 @@
+console.log('this is tutorial 53');
+function greet(name, greetText) {
+    let name1= "Name1";
+    console.log(greetText +name);
+    console.log(name + " is a good boy" );
+}
+function sum(a,b,c){
+    let d= a+b+c
+    return d
+}
+let greetText ="Good morning"
+let name = 'Shashwat';
+let name1 = 'Yash';
+let name2 = 'Kshitij';
+let name3 = 'Kishen';
+greet(name, greetText)
+greet(name1, greetText)
+greet(name2, greetText)
+greet(name3, greetText)
+let returnVal= sum(1,2,3)
+console.log(returnVal)
+// console.log(name + ' is a good boy')
+// console.log(name1 + ' is a good boy')
+// console.log(name2 + ' is a good boy')
+// console.log(name3 + ' is a good boy')
